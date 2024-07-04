dc9fa90628cf6878b8c7238d385b8ee5bc6fcfc0
vlad <rotariuvlad2001@gmail.com>
2024-07-05 01:56:50 +0300

    create initial project

---
+++ sports-network/src/test/java/com/vlad/sportsnetwork/SportsNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlad.sportsnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
