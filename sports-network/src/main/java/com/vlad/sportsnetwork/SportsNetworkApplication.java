5fd5f851d0538524dbe6d454f203fa38ce2f0646
vlad <rotariuvlad2001@gmail.com>
2024-07-05 14:51:36 +0300

    create user

--- sports-network/src/main/java/com/vlad/sportsnetwork/SportsNetworkApplication.java
+++ sports-network/src/main/java/com/vlad/sportsnetwork/SportsNetworkApplication.java
@@ -2,8 +2,10 @@ package com.vlad.sportsnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SportsNetworkApplication {
 
     public static void main(String[] args) {
