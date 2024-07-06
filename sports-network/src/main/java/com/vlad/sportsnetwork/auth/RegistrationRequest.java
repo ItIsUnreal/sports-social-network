37cc4ad1ef08b4a3f634705ae71a78be35322a10
vlad <rotariuvlad2001@gmail.com>
2024-07-06 16:18:42 +0300

    create auth controller

--- sports-network/src/main/java/com/vlad/sportsnetwork/auth/RegistrationRequest.java
+++ sports-network/src/main/java/com/vlad/sportsnetwork/auth/RegistrationRequest.java
@@ -1,8 +1,10 @@
 package com.vlad.sportsnetwork.auth;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,7 +23,13 @@ public class RegistrationRequest {
     @NotEmpty(message = "Lastname is mandatory")
     @NotBlank(message = "Lastname is mandatory")
     private String lastName;
+    @Email(message = "email is not formated")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+    @Size(min = 8, message = "passwrod too short")
+    @NotEmpty(message = "password is mandatory")
+    @NotBlank(message = "password is mandatory")
     private String password;
 
 }
